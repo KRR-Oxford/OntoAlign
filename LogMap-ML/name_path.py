e967704bff386b4cb00755561a12848c1310d581
Jiaoyan Chen <chen00217@gmail.com>
2021-08-25 16:40:37 +0100

    use uri_prefix of Label.py

--- LogMap-ML/name_path.py
+++ LogMap-ML/name_path.py
@@ -2,29 +2,13 @@ import argparse
 import json
 import math
 from owlready2 import *
+from lib.Label import uri_prefix
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--onto_file', type=str, default='data/foodon-merged.owl', help='data/helis_v1.00.owl')
 parser.add_argument('--name_file', type=str, default='data/foodon_class_name.json', help='data/helis_class_name.json')
 parser.add_argument('--path_file', type=str, default='data/foodon_all_paths.txt', help='data/helis_all_paths.txt')
 FLAGS, unparsed = parser.parse_known_args()
-
-'''
-The full namespace will be replaced by its abbreviation, to increase the readability of a class and its path.
-For namespaces not covered by this dictionary, the full namespace will be kept.
-'''
-namespaces = {'http://www.fbk.eu/ontologies/virtualcoach#': 'vc:',
-              'http://purl.obolibrary.org/obo/': 'obo:',
-              'http://bioontology.org/projects/ontologies/fma/fmaOwlDlComponent_2_0#': 'fma:',
-              'http://www.ihtsdo.org/snomed#': 'snomed:',
-              'http://www.orpha.net/ORDO/': 'ordo:'}
-
-
-def shorten_iri(iri):
-    for ns in namespaces:
-        if ns in iri:
-            return iri.replace(ns, namespaces[ns])
-    return iri
 
 
 def super_classes(c):
@@ -42,12 +26,12 @@ def get_class_name(o):
         name = c.name
         labels = c.label.en + c.label
         names = [name, labels[0]] if len(labels) > 0 else [name, None]
-        c_name[shorten_iri(iri=c.iri)] = names
+        c_name[uri_prefix(uri=c.iri)] = names
     return c_name
 
 
 def append_super_class(c, p):
-    p.append(shorten_iri(iri=c.iri))
+    p.append(uri_prefix(uri=c.iri))
     supclasses = super_classes(c=c)
     if owl.Thing in supclasses or len(supclasses) == 0 or supclasses is None:
         return p
